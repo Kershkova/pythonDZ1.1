64e4bc588596daf75ff43230c5a79ebb6fc2256a
Kershkova <124401535+Kershkova@users.noreply.github.com>
2023-04-13 23:51:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
+import re
+
+
 def parse(query: str) -> dict:
-    return {}
-
-
-if __name__ == '__main__':
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('http://example.com/') == {}
-    assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
-
-
-def parse_cookie(query: str) -> dict:
-    return {}
-
-
-if __name__ == '__main__':
-    assert parse_cookie('name=Dima;') == {'name': 'Dima'}
-    assert parse_cookie('') == {}
-    assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
+    d = {}
+    lenght = len(query)
+    index_1 = query.find('name')
+    print(index_1)
+    index_2 = query.find('color')
+    print(index_2)
+    if index_1 != -1 and index_2 != -1:
+        d = {}
+    if index_1 != -1 and index_2 != -1:
+        name = query[index_1 + 5 : index_2-1]
+        color = query[index_2 + 5 : lenght]
+        color = re.sub("[^A-Za-z]", "", color)
+        d["name"] = name
+        d["color"] = color
+    else:
+        if index_1 != -1 and index_2 == -1:
+            name = query[index_1 + 4: lenght]
+            name = re.sub("[^A-Za-z]", "", name)
+            d["name"] = name
+        else:
+            if index_1 == -1 and index_2 != -1:
+                color = query[index_2 + 5 : lenght]
+                color = re.sub("[^A-Za-z]", "", color)
+                d["color"] = color
+    return d
