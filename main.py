db41e1d63067513a333b5a20604b171274145b94
Kershkova <124401535+Kershkova@users.noreply.github.com>
2023-04-13 23:57:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ def parse(query: str) -> dict:
     d = {}
     lenght = len(query)
     index_1 = query.find('name')
-    print(index_1)
     index_2 = query.find('color')
-    print(index_2)
     if index_1 != -1 and index_2 != -1:
         d = {}
     if index_1 != -1 and index_2 != -1:
